2e4ed66101fcf32193c9fde4b338f9fa04a9f077
gledsonlucas111 <gledsonlucas111@gmail.com>
2025-01-08 13:05:31 -0300

    add thead and tbody

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,4 @@
-"use_client"
+"use_client";
 
 type typeBank = {
   id: number;
@@ -6,28 +6,30 @@ type typeBank = {
   compensation: number;
 };
 
-
 export default async function Home() {
   const data = await fetch("http://localhost:3001");
   const allbank = await data.json();
-  
+
   return (
     <div className="flex w-full justify-around">
       <table className="border">
-        <tr>
-          <th>Código de compensação</th>
-          <th>Nome Instituição</th>
-        </tr>
-        {allbank.map((bank: typeBank) => {
-          return (
-            <tr key={bank.id} className="border">
-              <td>{bank.compensation}</td>
-              <td>{bank.name}</td>
-            </tr>
-          );
-        })}
+        <thead>
+          <tr>
+            <th>Código de compensação</th>
+            <th>Nome Instituição</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allbank.map((bank: typeBank) => {
+            return (
+              <tr key={bank.id} className="border">
+                <td>{bank.compensation}</td>
+                <td>{bank.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
-
     </div>
   );
 }
